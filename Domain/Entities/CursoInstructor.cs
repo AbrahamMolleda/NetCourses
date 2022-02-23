8692815c1a9e53cfb1e2760f25ddfbd861f895ae
Abraham Azael <abraham@truiki.com>
2022-02-22 22:55:27 -0600

    Add project files.

---
+++ Domain/Entities/CursoInstructor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class CursoInstructor
+    {
+        public int CursoId { get; set; }
+        public Curso Curso { get; set; }
+        public int InstrcutorId { get; set; }
+        public Instructor Instructor { get; set; }
+    }
+}
---
+++ Persistence/CursosOnlineContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class CursosOnlineContext : DbContext
+    {
+        public CursosOnlineContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CursoInstructor>().HasKey(ci => new { ci.InstrcutorId, ci.CursoId });
+        }
+    }
+}
