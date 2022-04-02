8eb49e3388b8cc463810f1a0c3a4301ebeebf31b
Abraham Azael <aamol.riv@gmail.com>
2022-04-02 02:20:30 -0600

    Add: ComentariosController

---
+++ Application/Comentarios/Eliminar.cs
@@ -0,0 +1,47 @@
+﻿using Application.ManejadorError;
+using MediatR;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Comentarios
+{
+    public class Eliminar
+    {
+        public class Ejecuta : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+            private readonly CursosOnlineContext _context;
+            public Manejador(CursosOnlineContext context)
+            {
+                _context = context;
+            }
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var comentario = await _context.Comentario.FindAsync(request.Id);
+                if(comentario == null)
+                {
+                    throw new ManejadorExcepcion(HttpStatusCode.NotFound, new { mensaje = "No se encontro el comentario" });
+                }
+
+                _context.Comentario.Remove(comentario);
+                var resultado = await _context.SaveChangesAsync();
+                if(resultado > 0)
+                {
+                    return Unit.Value;
+                }
+
+                throw new Exception("No se pudo eliminar el comentario");
+            }
+        }
+    }
+}
---
+++ WebAPI/Controllers/ComentarioController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Comentarios;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComentarioController : MiControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Crear(Nuevo.Ejecuta data)
+        {
+            return await Mediator.Send(data);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Eliminar(Guid id)
+        {
+            return await Mediator.Send(new Eliminar.Ejecuta{ Id = id });
+        }
+    }
+}
